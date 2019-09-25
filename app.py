3e794ce5a8ac4fe19010dcceec5927721139181a
ahsin59 <38302279+ahsin59@users.noreply.github.com>
2019-09-25 04:03:28 +0100

    update readme

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key=os.urandom(24)
 
 
 app.config["MONGO_DBNAME"] = 'online_cookbookt'
-app.config["MONGO_URI"] = "mongodb+srv://root:root123456@myfirstcluster-jkztf.mongodb.net/online_cookbookt?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 
 mongo = PyMongo(app)
